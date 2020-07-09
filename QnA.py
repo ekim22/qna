4cb67480df79a6e4e9f38b7e72bc0974321f40ea
ekim22 <ekim2@ggc.edu>
2020-07-09 16:38:52 -0400

    Able to separate and organize headers, their questions, and the answers.

--- QnA.py
+++ QnA.py
@@ -28,34 +28,55 @@ def check_for_star(line):
 def check_for_header(line):
     return line.strip()[0].isalpha()
 
-
+pp.PrettyPrinter(indent=4)
 lines = []
+headers = {}
 
 with open(sys.argv[1]) as f:
     for line in f:
         if check_for_len(line) and check_for_header(line):
             print("H: " + line)
-        elif check_for_len(line) and check_for_dash(line):
-            print("Q: " + line)
-        elif check_for_len(line) and check_for_star(line):
-            print("A: " + line)
-            answers = []
-            answers.append(line.strip())
-            line = next(f)
-            if check_for_len(line) and check_for_dash(line):
-                print("Q: " + line)
-            while len(line.strip()) != 0 and line.strip()[0] != '-' and '*':
-                answers.append(line.strip())
-                print(line)
+            header = line.strip()
+            headers[header] = []
+            while check_for_len(line):
                 line = next(f)
                 if check_for_len(line) and check_for_dash(line):
                     print("Q: " + line)
-            answers = " ".join(answers)
-            lines.append(answers)
-        
+                    Q = line.strip()
+                    headers[header].append({Q: ""})
+                elif check_for_len(line) and check_for_star(line):
+                    answers = []
+                    while check_for_len(line) and not check_for_dash(line):
+                        answers.append(line.strip())
+                        print("A: " + line)
+                        line = next(f)
+                    answers = " ".join(answers)
+                    headers[header][0][Q] = answers
+                    if check_for_len(line) and check_for_dash(line):
+                        print("Q2: " + line)
+                        Q = line.strip()
+                        #headers[header].append({Q: ""})
+                
 
+       # elif check_for_len(line) and check_for_dash(line):
+       #     print("Q: " + line)
+       # elif check_for_len(line) and check_for_star(line):
+       #     answers = []
+       #     while len(line.strip()) != 0 and not check_for_dash(line):
+       #         answers.append(line.strip())
+       #         print("A: " + line)
+       #         line = next(f)
+       #     if check_for_len(line) and check_for_dash(line):
+       #         print("Q2: " + line)
+       #     answers = " ".join(answers)
+       #     lines.append(answers)
+       # 
 
-pp.PrettyPrinter(indent=4)
+for header in headers:
+    print("\n" + header)
+    for question, answer in headers[header][0].items():
+        print(question)
+        print("    " + answer)
 #pp.pprint(lines)
 #print(len(lines))
 #for line in lines:
