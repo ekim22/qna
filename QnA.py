7990af3ba6b4e2683ff43c1e83ee9ac59d2340c7
ekim22 <ekim2@ggc.edu>
2020-07-12 17:36:50 -0400

    Properly add answers to question sets. Created default interactive behavior.

--- QnA.py
+++ QnA.py
@@ -1,16 +1,4 @@
-# import sys
-# open file
-# read lines
-# if line doesnt start with whitespace its title
-# add to title dict
-# until next title or EOF
-    # create new dict
-    # if line starts with '-' add line as key
-    # elif line starts with '+' or number add line as value
-
-
 import sys
-import json
 import pprint as pp
 
 def check_for_len(line):
@@ -28,57 +16,133 @@ def check_for_star(line):
 def check_for_header(line):
     return line.strip()[0].isalpha()
 
-pp.PrettyPrinter(indent=4)
-lines = []
-headers = {}
 
+def list_all():
+    for header in headers:
+        for i in range(len(headers[header])):
+            pp.pprint(headers[header][i])
+
+
+def list_headers():
+    for header in headers:
+        print(header)
+
+
+def list_questions_and_answers():
+    for header in headers:
+        for question, answer in headers[header][0].items():
+            print("\n")
+            print(header)
+            print("Q: " + question)
+            print("A: " + answer)
+
+
+def list_qna_for_header():
+    my_printer = pp.PrettyPrinter(indent=4)
+    for topic in headers:
+        print(topic)
+        for question, answer in headers[topic][0].items():
+            print("Q: " + question[2:])
+            my_printer.pprint("A: " + answer[2:])
+        print("\n")
+
+
+def list_questions():
+    for header in headers:
+        for question, answer in headers[header][0].items():
+            print("Q: " + question)
+
+
+def list_answers():
+    for header in headers:
+        for question, answer in headers[header][0].items():
+            print("A:" + answer)
+
+
+def pose_questions():
+    # Default behavior should be list first header and its set of questions,
+    # allow inputting an answer, present next question and so on until all sets
+    # are done.
+    print("Select mode \n1. Default")
+    mode = input()
+    if mode == '1':
+        default()
+    elif not mode:
+        sys.exit(0)
+    
+
+def default():
+    my_printer = pp.PrettyPrinter(indent=2)
+    my_answers = {}
+    for header in headers:
+        print("= " * len(header) + header.upper() + " =" * len(header))
+        acknowledge = input("Skip? (y/n) ")
+        if acknowledge == 'n' or not acknowledge:
+            for index, question in enumerate(headers[header][0].keys()):
+                print(f"Question {index + 1}: {question}")
+                ans = input()
+                my_answers[question] = ans
+    my_printer.pprint(my_answers)
+    pose_questions()
+                
+
+
+
+headers = {} # A class should be made for a file and this should be an attr.
+
+# All this stuff should go in a function and be decomposed where similar, if
+# possible
 with open(sys.argv[1]) as f:
     for line in f:
         if check_for_len(line) and check_for_header(line):
-            print("H: " + line)
             header = line.strip()
             headers[header] = []
+            headers[header].append({})
+            line = next(f)
             while check_for_len(line):
-                line = next(f)
                 if check_for_len(line) and check_for_dash(line):
-                    print("Q: " + line)
-                    Q = line.strip()
-                    headers[header].append({Q: ""})
-                elif check_for_len(line) and check_for_star(line):
-                    answers = []
-                    while check_for_len(line) and not check_for_dash(line):
-                        answers.append(line.strip())
-                        print("A: " + line)
+                    questions = []
+                    while check_for_len(line) and not check_for_star(line):
+                        questions.append(line.strip())
                         line = next(f)
+                    question = " ".join(questions)
+                    headers[header][0][question] = ''
+                if check_for_len(line) and check_for_star(line):
+                    answers = []
+                    try:
+                        while check_for_len(line) and not check_for_dash(line):
+                            answers.append(line.strip())
+                            line = next(f)
+                    except StopIteration:
+                        answers = " ".join(answers)
+                        headers[header][0][question] = answers
+                        break
                     answers = " ".join(answers)
-                    headers[header][0][Q] = answers
-                    if check_for_len(line) and check_for_dash(line):
-                        print("Q2: " + line)
-                        Q = line.strip()
-                        #headers[header].append({Q: ""})
-                
+                    headers[header][0][question] = answers
 
-       # elif check_for_len(line) and check_for_dash(line):
-       #     print("Q: " + line)
-       # elif check_for_len(line) and check_for_star(line):
-       #     answers = []
-       #     while len(line.strip()) != 0 and not check_for_dash(line):
-       #         answers.append(line.strip())
-       #         print("A: " + line)
-       #         line = next(f)
-       #     if check_for_len(line) and check_for_dash(line):
-       #         print("Q2: " + line)
-       #     answers = " ".join(answers)
-       #     lines.append(answers)
-       # 
 
-for header in headers:
-    print("\n" + header)
-    for question, answer in headers[header][0].items():
-        print(question)
-        print("    " + answer)
-#pp.pprint(lines)
-#print(len(lines))
-#for line in lines:
-#    print("LN: " + line)
-#        
+
+while False:
+    print("Main Menu \n1. All \n2. Questions \n3. Answers \n4. Q&A's \n5."
+            "Headers \n6. Q&A per Header\n")
+    choice = input("Selection: ")
+    if choice == '1':
+        list_all()
+    elif choice == '2':
+        list_questions()
+    elif choice == '3':
+        list_answers()
+    elif choice == '4':
+        list_questions_and_answers()
+    elif choice == '5':
+        list_headers()
+    elif choice == '6':
+        list_qna_for_header()
+    else:
+        break
+
+while True:
+    pose_questions()
+
+# list_questions_and_answers()
+
